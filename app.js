2f60b45eb92f6188b8ee6dbf29fc89916eac3f2f
Matt Hsiung <xung@musician.com>
2016-01-19 15:58:49 -0500

    done?

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
 var express = require( 'express' );
+var swig = require('swig');
 var app = express();
+var routes = require( './routes/' );
+
+
+
+swig.setDefaults({ cache: false});
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', './views');
+
+//--------------START----------------
+app.use(express.static('public'))
+// app.use(function(req, res, next){
+// 	console.log(req.method)
+// 	console.log(req.url)
+// 	next()
+// });
+app.use('/', routes);
+
+
+
+
+//----------------END------------
+// app.use(function(req, res, next){
+// 	console.log('Status:' + res.statusCode)
+// });
 
 app.listen(3000, function (){
 	console.log('server listening')
 });
 
-app.use(function(req, res, next){
-	console.log(req.method)
-	console.log(req.url)
-
-	next()
-})
-
-app.use('/special/', function(req, res, next){
-	console.log('you reached speshul')
-	next()
-})
-
-app.get('/', function (req, res, next){
-	res.status(200)
-	
-	res.send('Welcome')
-	next()
-})
-
-app.use(function(req, res, next){
-	console.log('Status:' + res.statusCode)
-})
+// app.use('/special/', function(req, res, next){
+// 	console.log('you reached speshul')
+// 	next()
+// });
+// app.get('/:firstName/:lastName', function (req, res, next){
+// 	res.status(200)
+// 	var people = [{name: req.params.firstName}, {name: req.params.lastName}, {name: 'Son'}];
+// 	res.render( 'index', {title: 'Hall of Fame', people: people} );
+// 	next()
+// });
